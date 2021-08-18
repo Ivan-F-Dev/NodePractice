7679a318cb6a1d8af1fa72065ecc9c65df63d98e
Ivan <89821080113@mail.ru>
2021-08-18 22:09:50 +0300

    MERN 1:05

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')//dbuser:ivan dbuserpass:shitsoft
 
 const app = express()
-const PORT = process.env.PORT || 3001
+const PORT = config.get('port') || 3002
+
+app.use('/api/auth', require('./routes/auth.routes'))
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+        app.listen(PORT, () => console.log(`Server has been started on port ${PORT}...`))
+    } catch (e) {
+        console.log('Server Error:', e.message)
+        process.exit(1)
+    }
+}
+start()
 
 app.get('/', (req, res) => {
-    res.send('Hello world')
+    res.send('Hello from Express JS \"/\"')
 })
-
-app.get('/api', (req, res) => {
-    res.json({
-        message: "Hello from backend!"
-    })
-})
-
-app.listen(PORT, () =>
-    console.log(`Server has been started on port ${PORT}`)
-)
--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,17 @@
-import React, {useState, useEffect} from 'react'
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter} from "react-router-dom";
+import 'materialize-css'
+import {useRoutes} from "./routes";
 
 function App() {
-
-  const [data, setData] = useState(null)
-
-  useEffect(() => {
-    fetch('/api')
-        .then(response => response.json())
-        .then(response => setData(response.message))
-  }, [])
-
+    const routes = useRoutes(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {
-            !data ? "Loading..." : data
-          }
-        </p>
-
-      </header>
+    <div className='container'>
+        <BrowserRouter>
+            {routes}
+        </BrowserRouter>
     </div>
-  );
+  )
 }
 
 export default App;
